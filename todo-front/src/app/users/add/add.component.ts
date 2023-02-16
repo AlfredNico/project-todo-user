245db2d61d147a0d229549209c2ee991ab41a08d
Alfred Nico <alfrednicotsu@gmail.com>
2023-02-16 13:47:06 +0300

    correction confirm password

--- todo-front/src/app/users/add/add.component.ts
+++ todo-front/src/app/users/add/add.component.ts
@@ -3,6 +3,7 @@ import { UserService } from 'src/app/services/user.service';
 import { FormBuilder, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { SnackBarService } from 'src/app/shared/services/snack-bar.service';
+import { CustomValidationService } from 'src/app/shared/services/custom-validation.service';
 
 @Component({
   selector: 'app-add',
@@ -12,19 +13,29 @@ import { SnackBarService } from 'src/app/shared/services/snack-bar.service';
 export class AddComponent implements OnInit {
   hidePwd = true;
 
-  public form = this.fb.group({
-    username: ['', Validators.required],
-    firstname: ['', Validators.required],
-    lastname: ['', Validators.required],
-    password: ['', Validators.minLength(6)],
-  });
+  public form = this.fb.group(
+    {
+      username: ['', Validators.required],
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      password: ['', Validators.minLength(6)],
+      confirm_password: ['', Validators.minLength(6)],
+    },
+    {
+      validator: this.custumValidator.MatchPassword(
+        'password',
+        'confirm_password'
+      ),
+    }
+  );
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     private dialogRef: MatDialogRef<AddComponent>,
     private fb: FormBuilder,
     private snackbar: SnackBarService,
-    private usrService: UserService
+    private usrService: UserService,
+    private custumValidator: CustomValidationService
   ) {
     if (this.data.data) {
       this.form.patchValue({
@@ -40,16 +51,25 @@ export class AddComponent implements OnInit {
 
   save() {
     if (this.form.valid) {
+      const { username, firstname, lastname, password } = this.form.value;
+
       if (this.data.action === 'add') {
-        this.usrService.add(this.form.value).subscribe((result) => {
-          if (result && result.message) {
-            this.snackbar.sucess(result.message);
-            this.dialogRef.close(true);
-          }
-        });
+        this.usrService
+          .add({ username, firstname, lastname, password })
+          .subscribe((result) => {
+            if (result && result.message) {
+              this.snackbar.sucess(result.message);
+              this.dialogRef.close(true);
+            }
+          });
       } else {
         this.usrService
-          .update(this.data.data.id, this.form.value)
+          .update(this.data.data.id, {
+            username,
+            firstname,
+            lastname,
+            password,
+          })
           .subscribe((result) => {
             if (result && result.message) {
               this.snackbar.sucess(result.message);
